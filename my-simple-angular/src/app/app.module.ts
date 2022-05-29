4d384366a8cb6c5ba2e0be231c0b602ffe116bb9
Nour <nour.trabelsi@charguia.r-iset.tn>
2022-05-29 13:37:16 +0100

    first commit

---
+++ my-simple-angular/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth/auth.module';
+import { ProfileComponent } from './main/profile/profile.component';
+import { InterceptorService } from './services/interceptor-service.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    AuthModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
